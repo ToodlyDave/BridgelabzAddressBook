5f9868114d8810770550524232c51a5d9e3f2072
ToodlyDave <alapatdavid@gmail.com>
2021-12-27 15:15:29 +0530

    OPTIMISE code

--- src/com/address/jdbc/Statements.java
+++ src/com/address/jdbc/Statements.java
@@ -4,39 +4,39 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
-import com.address.entity.AddressBook;
 import com.address.entity.ContactPerson;
-import com.address.jdbc.entites.AddressBookDB;
 
 public class Statements {
 
 	final String READ_QUERY = "SELECT ab.first_name, ab.last_name, a.address, a.city, a.state, a.zip, "
 			+ "ab.phone_number, ab.email FROM address_book ab LEFT JOIN address a ON ab.first_name = a.first_name";
 
-	public AddressBook readAddressBook() {
+	public ArrayList<ContactPerson> readAddressBook() {
 
 		Connection connection = Config.connection;
+		ArrayList<ContactPerson> addressBookDB = new ArrayList<>();
 
 		try {
 			Statement fetchStatement = connection.createStatement();
 			ResultSet result = fetchStatement.executeQuery(READ_QUERY);
 
 			while (result.next()) {
-				AddressBookDB.getAddressBookDB()
+				addressBookDB
 						.add(new ContactPerson(result.getString(1), result.getString(2),
 								result.getString(3), result.getString(4), result.getString(5), result.getInt(6),
 								result.getInt(7), result.getString(8)));
 			}
 			
 			System.out.println(" The stuff in the db: ");
-			System.out.println(AddressBookDB.getAddressBookDB());
+			System.out.println(addressBookDB);
 
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-		return null;
+		return addressBookDB;
 	}
 }
--- src/com/address/test/Testing.java
+++ src/com/address/test/Testing.java
@@ -1,27 +1,21 @@
 package com.address.test;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import java.sql.Connection;
 import java.util.ArrayList;
-import java.util.Iterator;
 
 import org.junit.Test;
 
 import com.address.entity.ContactPerson;
-import com.address.jdbc.Config;
 import com.address.jdbc.Statements;
-import com.address.jdbc.entites.AddressBookDB;
 
 public class Testing {
 
 	@Test
 	public void testConnectionHappy() {
-		Connection connection = Config.connection;
 		boolean comparison = true;
 		Statements queries = new Statements();
-		queries.readAddressBook();
+		ArrayList<ContactPerson> resultDB = queries.readAddressBook();
 		
 
 		ArrayList<ContactPerson> arrayForComparison = new ArrayList<>();
@@ -31,8 +25,9 @@ public class Testing {
 				.add(new ContactPerson("James", "Rollins", "US", "Brooklyn", "NY", 456, 1987456129, "hey@gmail.com"));
 		arrayForComparison.add(new ContactPerson("Bill", "Gates", "US", "SF", "Cali", 159, 567892139, "win@gmail.com"));
 
+		
 		for (int i = 0; i < arrayForComparison.size(); i++) {
-			if (!arrayForComparison.get(i).equalsCustom(AddressBookDB.getAddressBookDB().get(i))) {
+			if (!arrayForComparison.get(i).equalsCustom(resultDB.get(i))) {
 				comparison = false;
 				break;
 			}
